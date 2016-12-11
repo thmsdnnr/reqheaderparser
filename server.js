2fdcfdfd5c1d6b8bb0cb912be2725ee704a1fb2d
thomas danner <thmsdnnr@gmail.com>
2016-12-11 19:02:27 +0000

    First working commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express=require('express');
+var app=express();
+app.get('/', function(req,res){
+res.send({"IP address":req.headers['x-forwarded-for'],"language":req.headers['accept-language'].split(',')[0],"software":req.headers['user-agent']});
+res.end();
+});
+app.listen(8080, function(){console.log('listening on port 8080...');});
