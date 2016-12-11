b3c8e92f5a0ff9799119eb93356bc0951ad03082
thomas danner <thmsdnnr@gmail.com>
2016-12-11 19:16:53 +0000

    updates for heroku

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ app.get('/', function(req,res){
 res.send({"IP address":req.headers['x-forwarded-for'],"language":req.headers['accept-language'].split(',')[0],"software":req.headers['user-agent']});
 res.end();
 });
+var port = process.env.PORT||8080;
 app.listen(8080, function(){console.log('listening on port 8080...');});
